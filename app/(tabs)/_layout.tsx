4af3dc7ee8ca86aeba2520e76d65c5dd4fa62e6d
Samson <samosonjayaraju@gmail.com>
2025-12-19 00:23:26 +0530

    Added Home Icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,15 @@
 import { Tabs } from "expo-router";
-
+import AntDesign from "@expo/vector-icons/AntDesign";
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: () => <AntDesign name="home" size={24} color="black" />,
+        }}
+      />
       <Tabs.Screen name="login" options={{ title: "Login" }} />
     </Tabs>
   );
